55c2fd7dff0d11241fd06b1f845d4d3e93d1a65b
Mustafa Mohebi <128157702+MMOHEB1@users.noreply.github.com>
2024-05-29 16:22:59 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from tkinter import messagebox
+import json
+
+
+
+
+
+
+# ---------------------------- UI SETUP ------------------------------- #
+screen = Tk()
+screen.config(width=500, height=500)
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.mainloop()
